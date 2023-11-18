317f0372670fcf8a7a1929815b0121c2bee9e09a
peilunnn <peilunnn@gmail.com>
2023-11-18 19:30:45 +0800

    fix rendering logic to still render entities that do not have an Animation comopnent

--- GameTest/App/Systems/src/RenderEntities.cpp
+++ GameTest/App/Systems/src/RenderEntities.cpp
@@ -7,10 +7,15 @@ void RenderEntities::Render(EntityManager& entityManager) {
         auto renderable = entityManager.GetComponent<Renderable>(entity);
         auto animation = entityManager.GetComponent<Animation>(entity);
 
-        if (transform && renderable && animation) {
+        if (transform && renderable) {
             renderable->sprite->SetPosition(transform->position.x, transform->position.y);
             renderable->sprite->SetScale(transform->scale.x);
             renderable->sprite->SetAngle(transform->rotation.z);
+            
+            if (animation) {
+                renderable->sprite->SetAnimation(animation->currentAnimation);
+            }
+
             renderable->sprite->SetAnimation(animation->currentAnimation);
             renderable->sprite->Draw();
         }
